37fddbecaf1188989a233269e57249c1ad296fb4
ArcticFoox <jongsin16@naver.com>
2023-12-09 23:57:23 +0900

    23073
    string
    https://www.acmicpc.net/problem/23037

---
+++ five.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+	int n, sum = 0, arr[5] = { 0 };
+	for (int i = 0; i < 5; i++) {
+		scanf("%1d", &arr[i]);
+		sum += pow(arr[i], 5);
+	}
+
+	printf("%d", sum);
+}
